b00673b755a1f3fb43c2283a7dcc77f26ecbbfd6
ding005 <137453637@qq.com>
2021-07-06 18:50:27 +0800

    箱子移动  005_2(后续)

--- MMO/Assets/Script/CSharp/xLuaFramework/RoleCtrl.cs
+++ MMO/Assets/Script/CSharp/xLuaFramework/RoleCtrl.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 //==============================
 //两个物体都必须带有碰撞器(Collider)，其中一个物体还必须带有Rigidbody刚体。
 //lookAt  主要是物体的z轴朝着目标点
-public class Move : MonoBehaviour {
+public class RoleCtrl : MonoBehaviour {
 
     //不赋值 默认的是vector3.zero
     public Vector3 m_targetPos;
@@ -18,52 +18,73 @@ public class Move : MonoBehaviour {
     private CharacterController m_CharacterController;
 
     [SerializeField]
-    private float m_speed = 1;
-    // Use this for initialization
+    private float m_Speed = 1;
+
+    /// <summary>
+    /// 转身速度
+    /// </summary>
+    private float m_RotationSpeed = 0.2f;
+
+
+    /// <summary>
+    /// 转身方向
+    /// </summary>
+    private Quaternion m_Quaternion;
+
+    private float gravity = 20f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
     void Start ()
     {
         m_CharacterController = gameObject.transform.GetComponent<CharacterController>();
-
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
+            Debug.Log("鼠标位置 == "+ Input.mousePosition);
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitinfo;
             //碰到物体会返回true
-            if (Physics.Raycast(ray,out hitinfo))
+            if (Physics.Raycast(ray, out hitinfo))
             {
                 //CurrentCultureIgnoreCase 不区分大小写
-                if (hitinfo.collider.gameObject.name.Equals("Ground",System.StringComparison.CurrentCultureIgnoreCase))
+                if (hitinfo.collider.gameObject.name.Equals("Ground", System.StringComparison.CurrentCultureIgnoreCase))
                 {
                     m_targetPos = hitinfo.point;
-                    Debug.Log(m_targetPos);
+                    //Debug.DrawLine(Camera.main.transform.position, m_targetPos);
                 }
             }
+        }
+        if (m_targetPos != Vector3.zero)
+        {
+            Debug.DrawLine(Camera.main.transform.position, m_targetPos);
         }
         //让角色贴着地面
         if (!m_CharacterController.isGrounded)
         {
-            m_CharacterController.Move((transform.position + new Vector3(0,-1000,0))- transform.position);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= m_Speed;
         }
         if (m_targetPos != Vector3.zero)
         {
             //不能是零 因为移动的时候有小数 导致来回颤动
-            if (Vector3.Distance(m_targetPos,transform.position) >0.1f)
+            if (Vector3.Distance(m_targetPos, transform.position) > 0.1f)
             {
-                Vector3 director = m_targetPos - transform.position;
-                director = director.normalized; //归一化
-                Debug.Log("director = " + director + " Time.deltaTime = " + Time.deltaTime + " m_speed = " + m_speed);
-                director = director * Time.deltaTime * m_speed;
-                //Debug.Log("director = " + director);
 
-                transform.LookAt(m_targetPos,Vector3.up);
+                Vector3 director = m_targetPos - transform.position;
+                director = director.normalized; //归一化 方向上x y z长度为1
+                director = director * Time.deltaTime * m_Speed;
+                //transform.LookAt(m_targetPos, Vector3.up);
                 m_CharacterController.Move(m_targetPos);
             }
-            
+
         }
-	}
+        moveDirection.y -= gravity * Time.deltaTime;//模拟重力
+        m_CharacterController.Move(moveDirection * Time.deltaTime);//移动
+
+    }
 }
